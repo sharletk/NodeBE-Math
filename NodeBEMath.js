7375b07cdf3d2e4877b823bcf5ef9757fbb814be
SharletP <sharletman@gmail.com>
2019-08-07 02:56:29 +0300

    Added NodeBEMath

---
+++ NodeBEMath.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  Facing: require("./src/Facing.js"),
+  VectorMath: require("./src/VectorMath.js"),
+  Vector2: require("./src/Vector2.js"),
+  Vector3: require("./src/Vector3.js")
+};
